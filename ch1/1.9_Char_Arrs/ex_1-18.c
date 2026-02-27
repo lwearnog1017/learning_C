c82c3c8bffbe6d3d48528ffacafc9f843fdbd947
Lero Wang <lwearnog1017@gmail.com>
2026-02-26 18:27:42 -0800

    exercise 1.18 completed

--- ch1/1.9_Char_Arrs/ex_1-18.c
+++ ch1/1.9_Char_Arrs/ex_1-18.c
@@ -1,13 +1,9 @@
 /*
 Write a program to remove trailing blanks and tabs from 
 each line of input, and to delete entirely blank lines.
-
-APPROACH:
-
-
 */
 #include <stdio.h>
-#define MAXLINE 30
+#define MAXLINE 100
 #define IP 0
 #define DONE 1
 
@@ -16,30 +12,15 @@ void removetrailing(char line[], int size);
 
 int main() {
 
-    /* run this loop until we hit a line that contains EOF. in that case if 
-    the line had other characters before EOF we want to printf those chars + \n
-    otherwise just print \n. to check if the line contains EOF, run*/
     char line[MAXLINE];
     int size;
 
     
     while ((size = nextline(line, MAXLINE)) != 0) {
         removetrailing(line, size);
-        printf("%s\n", line);
+        if (line[0] != 0)
+            printf("%s\n", line);
     }
-    
-
-    /* Testing removetrailing()
-    line[0] = '\t';
-    line[1] = 'a';
-    line[2] = 'b';
-    line[3] = '\t';
-    line[4] = '\n';
-
-    removetrailing(line,5);
-    printf("%s", line);
-    */
-    
 
 }
 
@@ -52,15 +33,6 @@ int nextline(char line[], int maxline) {
             ++len;
         }
     }
-    /*
-    if (c=='\n')
-        if (i == maxline-1)
-            line[i-1] = c;
-        else
-            line[i++] = c;
-    
-    line[i] = 0;
-    */
 
     if (c=='\n')
         line[len++] = '\n';
@@ -69,26 +41,21 @@ int nextline(char line[], int maxline) {
 }
 
 void removetrailing(char line[], int size) {
-    int i, begin, end, bprog, eprog, bitem, eitem, len;
-    begin = 0;
-    end = size - 1;
-    bprog = eprog = IP;
-    for (i = 0; i < size && (bprog == IP || eprog == IP); i++) {
-        if (bprog == IP && (bitem = line[i]) != ' ' && bitem != '\t' && bitem != '\n') {
-            bprog = DONE;
-            begin = i;
-        }
-        if (eprog == IP && (eitem = line[end - i]) != ' ' && eitem != '\t' && eitem != '\n') {
-            eprog = DONE;
-            end = end - i;
-        }
-    }
-    len = end - begin + 1;
-    for (i = 0; i < len; i++) {
-        line[i] = line[i + begin];
-    }
-    
-    line[i] = '\0';
-}
 
-// DON"T HAVE TO KEEP TRACK OF END JUST KEEP TRACK OF LENGTH
+    int begin = 0, end = size-1, c, len;
+
+    for (int i = 0; i < size - 1 && ((c = line[i]) == ' ' || c == '\t' || c == '\n'); i++)
+        begin++;
+    
+    for (int i = size-1; i > 0 && i > begin && ((c = line[i]) == ' ' || c == '\t' || c == '\n'); i--)
+        end--;
+
+    len = end - begin + 1;
+    for (int i = 0; i < len; i++) {
+        line[i] = line[i+begin];
+    }
+
+    line[len] = 0;
+    if (line[0] == ' ' || line[0] == '\t' || line[0] == '\n')
+        line[0] = 0;
+}
