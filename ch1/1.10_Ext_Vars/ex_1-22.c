5a30a1a5c2881ae1bd45ccba5702352d6c9ece75
Lero Wang <lwearnog1017@gmail.com>
2026-02-28 17:23:30 -0800

    finished exercise 1-22

--- ch1/1.10_Ext_Vars/ex_1-22.c
+++ ch1/1.10_Ext_Vars/ex_1-22.c
@@ -5,27 +5,74 @@ intelligent with very long lines, and if there are no blanks or
 tabs before the specified column. */
 
 #include <stdio.h>
-#define MAXCOL 70
+#define MAXCOL 10
 #define MAXWORD 200
 #define TABSTOP 4
 #define IN 0
 #define OUT 1
+#define FINISHED 400
+
+void printword(char word[], int wordlen);
+// void getword(char word[]);
+
+int col = 0;
 
 int main() {
     char word[MAXWORD];
-    int c, col = 0, status = IN;
-    while ((c = getchar()) != EOF) {
-        if (c == ' ' || c == '\t') {
+    int c, status = 2, wordidx = 0, done = 0;
+
+    while (done != FINISHED) {
+        c = getchar();
+        if (c == ' ' || c == '\t' || c == '\n' || c == EOF) {
+            if (status == IN) {
+                printword(word, wordidx);
+            }
             if (c == ' ')
                 ++col;
-            else
+            else if (c == '\t')
                 col += TABSTOP - (col % TABSTOP);
+            else
+                col = 0;
+            if (c == EOF)
+                done = FINISHED;
+            else
+                putchar(c);
             status = OUT;
         } else {
-            if (status == OUT) {
-
-            }
+            if (status == OUT)
+                wordidx = 0;
+            word[wordidx++] = c;
+            status = IN;
+            ++col;
         }
     }
-
 }
+
+void printword(char word[], int wordlen) {
+    word[wordlen] = '\0';
+    if (col > MAXCOL && wordlen <= MAXCOL) {
+        printf("\n%s", word);
+        col = wordlen;
+    } else if (wordlen > MAXCOL) {
+        if (col - wordlen > 0)
+            putchar('\n');
+        col = 0;
+        for (int i = 0; i < wordlen; ++col) {
+            //printf("%d", col);
+            if (col == MAXCOL) {
+                putchar('\n');
+                col = 0;
+            }
+            putchar(word[i++]);
+        }
+    } else {
+        printf("%s", word);
+    }
+}
+
+/* gets all chars from after the end of the previous "word", including blank space
+void getword(char word[]) {
+    for (int c, i = 0, start; (c = getchar()) != ' ' && c != '\t' && c != '\n'; i++) {
+        word[i] = c;
+    }
+} */
